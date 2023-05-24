2c73a8fa5abe8a8374af29efd167ed37c1a1c678
sentido1337 <abusentidoramaldavida@gmail.com>
2023-05-24 15:58:49 -0300

    recweb2

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,33 @@ const carrinho = ref({
   total: 0
 })
 
+function removerItemCarrinho(item) {
+  const index = carrinho.value.itens.findIndex((i) => i.id === item.id)
+  carrinho.value.total -= item.total
+  carrinho.value.itens.splice(index, 1) 
+}
+
+function atualizaQuantidadeItem(item) {
+  carrinho.value.total -= item.total
+  item.total = item.price * item.quantidade
+  carrinho.value.total += item.total
+}
+
 function adicionarAoCarrinho(livro) {
   const index = carrinho.value.itens.findIndex((item) => item.id === livro.id)
   if (index === -1) {
-  carrinho.value.itens.push({
-    ...livro,
-    quantidade: 1,
-    total: livro.price
-  })
-  carrinho.value.total += livro.price
-} else {
-  carrinho.value.itens[index].quantidade++
-  carrinho.value.itens[index].total += livro.price
-  carrinho.value.total += livro.price
+    carrinho.value.itens.push({
+      ...livro,
+      quantidade: 1,
+      total: livro.price
+    })
+    carrinho.value.total += livro.price
+  } else {
+    carrinho.value.itens[index].quantidade++
+    carrinho.value.itens[index].total += livro.price
+    carrinho.value.total += livro.price
+  }
 }
-}
-
 
 function formatarPreco(preco) {
   return 'R$ ' + preco.toFixed(2).replace('.', ',')
@@ -53,13 +64,22 @@ function formatarPreco(preco) {
               <img :src="item.img" class="icon-capa-livro" />
             </div>
             <div class="detalhes-livro">
-              
               <div>
                 <p>{{ item.title }}</p>
                 <p class="info-livro-preco">{{ formatarPreco(item.price) }}/un</p>
               </div>
               <div>
-                <p>Qtde: {{ item.quantidade }}</p>
+                <p>
+                  Quantidade:
+                  <input
+                    type="number"
+                    v-model="item.quantidade"
+                    @change="atualizaQuantidadeItem(item)"
+                    min="1"
+                  />
+                  {{ item.quantidade }}
+                </p>
+                <button @click="removerItemCarrinho(item)">Remover</button>
                 <p>Total: {{ formatarPreco(item.total) }}</p>
               </div>
             </div>
